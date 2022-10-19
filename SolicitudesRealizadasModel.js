d07c860884eefaba95cf9f7fa4d4b1343cee07a2
llZeviL <115958885+llZeviL@users.noreply.github.com>
2022-10-19 01:29:12 -0400

    Update SolicitudesRealizadasModel.js

--- SolicitudesRealizadasModel.js
+++ SolicitudesRealizadasModel.js
@@ -4,9 +4,9 @@ import db from "../database/db.js"
 
 cons Solicitud_RealizadaModel=db.define('Solicitud_Realizada',{
     Estado:{type:Datatypes.STRING},
-    Fecha_Pregunta:{type:Datatypes.INTEGER}
-    ID_Solicitud:{type:Datatypes.INTEGER}
+    Fecha_Pregunta:{type:Datatypes.INTEGER},
+    ID_Solicitud:{type:Datatypes.INTEGER},
     ID_Estudiante:{type:Datatypes.INTEGER}
 })
 
-export default Pregunta_RealizadaModel
+export default Solicitud_RealizadaModelModel
